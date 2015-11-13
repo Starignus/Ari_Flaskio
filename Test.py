36a4b3953f2f0a3ea17d5ce151e9be3cffd034fe
Starignus <ariadna@starignus.com>
2015-11-13 18:46:51 +0000

    First test

---
+++ Test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
